0391e594a796ce4cfeb4aeabd9206fd88a51acca
Özcan Tuncer <97348094+tuncerozcan90@users.noreply.github.com>
2023-07-10 09:44:05 +0300

    Add project files.

---
+++ WebApiProject/Controllers/BooksController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIProject.Models;
+using WebAPIProject.Repositories;
+using WebAPIProject.Models;
+using WebAPIProject.Repositories;
+
+namespace WebAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book?>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(PostBooks), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            await _bookRepository.Update(book);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBooks(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+            if (bookToDelete == null)
+                return BadRequest();
+            await _bookRepository.Delete(bookToDelete.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ WebApiProject/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPIProject.Models;
+
+namespace WebAPIProject.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book?> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
